5134c84a91409bfee034df034b8c6a59e6df0b8a
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-01-01 18:15:16 +0000

    Added login system.

--- src/Bank-ATM-Simulator.cpp
+++ src/Bank-ATM-Simulator.cpp
@@ -4,7 +4,16 @@
 using namespace std;
 
 bool login(){
-    
+    int pin_number = NULL;
+
+    cout << "Please input your pin number.";
+    cin >> pin_number;
+
+    if(pin_number == 1234){
+        return true;
+    } else{
+        return false;
+    }
 }
 
 int main(){
