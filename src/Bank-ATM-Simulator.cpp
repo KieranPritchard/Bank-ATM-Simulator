d021c7a8b2314720fe3561e9209e079948b79051
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-01-01 18:34:15 +0000

    Added comments and a pointer.

--- src/Bank-ATM-Simulator.cpp
+++ src/Bank-ATM-Simulator.cpp
@@ -3,8 +3,9 @@
 
 using namespace std;
 
+// Makes the user login to the program
 bool login(){
-    int pin_number = NULL;
+    int pin_number = 0;
 
     cout << "Please input your pin number.";
     cin >> pin_number;
@@ -17,17 +18,26 @@ bool login(){
 }
 
 int main(){
+
+    // Keeps looping until login() is true
     while (login() == false){
         login();
     }
-    
+
+    // Loops intill option 4 is used
     while (true){
+        // Displayed text
         cout << "Please select your option:\n";
         cout << "(1) Balance Inquiry.\n";
         cout << "(2) Deposit Money.\n";
         cout << "(3) Withdraw Money.\n";
         cout << "(4) Log Out.\n";
 
+        // Balence pointer
+        int balence;
+        int* ptr = &balence;
+
+        // Switch statement allows the user to access different functions of the program
         int choice = 0;
 
         switch (choice){
