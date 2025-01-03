3dffa7b32fd31535d96d323f11a1774d4fcb33f1
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-01-03 17:30:24 +0000

    Added text and formatting for more context.

--- src/Bank-ATM-Simulator.cpp
+++ src/Bank-ATM-Simulator.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool login(){
     int pin_number = 0;
 
-    cout << "Please input your pin number.";
+    cout << "Please input your pin number: ";
     cin >> pin_number;
 
     if(pin_number == 1234){
@@ -27,13 +27,20 @@ void deposit_money(int* ptr){
     
     cin >> deposit_amount;
     *ptr += deposit_amount;
+    cout << "Deposit Successful. Your Balance is £" << *ptr;
 }
 
 void withdraw_money(int* ptr){
     int withdrawal_amount = 0;
     cout << "Please input the amount of money you want to withdraw: ";
     cin >> withdrawal_amount;
-    *ptr -= withdrawal_amount;
+    if(*ptr < 0){
+        *ptr -= withdrawal_amount;
+        cout << "Withdrawal Successful. Your Balance is £" << *ptr;
+
+    } else{
+        cout << "Insuffient balance. Withdrawal Failed.";
+    }
 }
 
 int main(){
@@ -52,13 +59,17 @@ int main(){
     // Loops intill option 4 is used
     while (choice != 4){
         // Displayed text
+        cout << "-----------------------------";
+        cout << "What would you like to-do.";
+        cout << "-----------------------------";
         cout << "(1) Balance Inquiry.\n";
         cout << "(2) Deposit Money.\n";
         cout << "(3) Withdraw Money.\n";
         cout << "(4) Log Out.\n";
-
-        cout << "Please select your option:\n";
+        cout << "-----------------------------";
+        cout << "Please select your option:";
         cin >> choice;
+        cout << "-----------------------------";
 
         switch (choice){
         case 1:
@@ -74,9 +85,11 @@ int main(){
             break;
 
         case 4:
+            cout << "Thank you for your custom.";
             break;    
 
         default:
+            cout << "Invaild option. Please try again.";
             break;
         }
     }
